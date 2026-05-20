2ab587e898683428b7a3a13434297c30b6cb8aee
maverickdeepak <imdeepakchauhan12@gmail.com>
2026-05-20 09:42:14 +0530

    fix: update CartApp import to reference CartPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense, lazy, useEffect } from "react";
 import ErrorBoundary from "./ErrorBoundary";
 
-const CartApp = lazy(() => import("cartApp/CartPageBROKEN"));
+const CartApp = lazy(() => import("cartApp/CartPage"));
 const ProductApp = () => {
   useEffect(() => {
     import("productApp/ProductElement");
