be3b0d83665ee682095e17e2d03e17d8cf811cf9
maverickdeepak <imdeepakchauhan12@gmail.com>
2026-05-20 09:16:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Suspense, lazy, useEffect } from "react";
+
+const CartApp = lazy(() => import("cartApp/CartPage"));
+const ProductPage = () => {
+  useEffect(() => {
+    import("productApp/ProductElement");
+  }, []);
+
+  return <product-app></product-app>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Shell App</h1>
+      <Suspense fallback={<div>Loading Cart Page...</div>}>
+        <CartApp />
+      </Suspense>
+      <ProductPage />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index",
+
+  output: {
+    publicPath: "auto",
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { modules: false }],
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "shell",
+      remotes: {
+        cartApp: "cartApp@https://d1jdjsmwpwk5wc.cloudfront.net/remoteEntry.js",
+        productApp:
+          "productApp@https://d32mpbk2wjq13i.cloudfront.net/remoteEntry.js",
+      },
+      shared: {
+        react: { singleton: true, eager: true },
+        "react-dom": { singleton: true, eager: true },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
